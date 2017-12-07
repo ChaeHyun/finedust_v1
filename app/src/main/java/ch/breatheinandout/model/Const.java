60de7b4a1e500be3656df1bdda44f79186ceb80a
ChaeHyun <iamrch@naver.com>
2017-12-08 02:33:55 +0900

    commit test

--- app/src/main/java/ch/breatheinandout/model/Const.java
+++ app/src/main/java/ch/breatheinandout/model/Const.java
@@ -6,7 +6,7 @@ import ch.breatheinandout.R;
 
 
 public class Const {
-    public static final String SERVICEKEY = "w7R0iLcYWCyiGgCoLaoR4WiGUZMF3tpUwJNIdlsKT%2FSqFnpzhA48By8T3eTkbypNn1P%2BhzMFVeKPHI4rgMvGew%3D%3D";   //SH
+    public static final String SERVICEKEY = "w7R0iLcYWCyiGgCoLaoR4WiGUZMF3tpUwJNIdlsKT%2FSqFnpzhA48By8T3eTkbypNn1P%2BhzMFVeKPHI4rgMvGew%3D%3D";
 
     public static final String DAUM_API_KEY = "f9946de16d015bc728f1d55ceafe0c2e&libraries=services";
 
